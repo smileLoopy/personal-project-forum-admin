3a10d2d0af37fe0baad70bd318f0e21c98cb6393
smile_loopy <smile.loopys2@gmail.com>
2024-08-12 18:38:46 -0700

    #4- Initialize Spring Boot Project
    Based on the information we wrote on #2,  wrote codes to initialize basic frame of Spring Boot.
    
    Checked the operation.

---
+++ src/test/java/com/personal/projectforumadmin/PersonalProjectForumAdminApplicationTests.java
@@ -0,0 +1,15 @@
+package com.personal.projectforumadmin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@ActiveProfiles("test")
+@SpringBootTest
+class PersonalProjectForumAdminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
