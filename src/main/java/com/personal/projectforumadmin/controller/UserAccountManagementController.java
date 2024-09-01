f99af5c53baed97256941ddca2558ca6112874fa
smile_loopy <smile.loopys2@gmail.com>
2024-08-31 20:35:27 -0700

    #20 - Implement controller, service, view of user account addmin page

--- src/main/java/com/personal/projectforumadmin/controller/UserAccountManagementController.java
+++ src/main/java/com/personal/projectforumadmin/controller/UserAccountManagementController.java
@@ -1,28 +1,49 @@
 package com.personal.projectforumadmin.controller;
 
 
+import com.personal.projectforumadmin.dto.response.UserAccountResponse;
+import com.personal.projectforumadmin.service.UserAccountManagementService;
 import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
+@RequiredArgsConstructor
 @RequestMapping("/management/user-accounts")
 @Controller
 public class UserAccountManagementController {
 
+    private final UserAccountManagementService userAccountManagementService;
+
     @GetMapping
     public String userAccounts(
-            @PageableDefault(size = 10, sort = "createdAt", direction = Sort.Direction.DESC) Pageable pageable,
             HttpServletRequest request,
             Model model
     ) {
         String requestURI = request.getRequestURI();
         model.addAttribute("isUserAccounts", "/management/user-accounts".equals(requestURI));
+        model.addAttribute(
+                "userAccounts",
+                userAccountManagementService.getUserAccounts().stream().map(UserAccountResponse::from).toList()
+        );
         return "management/user-accounts";
+    }
+
+    @ResponseBody
+    @GetMapping("/{userId}")
+    public UserAccountResponse userAccount(@PathVariable String userId) {
+        return UserAccountResponse.from(userAccountManagementService.getUserAccount(userId));
+    }
+
+    @PostMapping("/{userId}")
+    public String deleteUserAccount(@PathVariable String userId) {
+        userAccountManagementService.deleteUserAccount(userId);
+
+        return "redirect:/management/user-accounts";
     }
 
 }
