23964c94b035a83a12d11af5222a96e2a07065f8
smile_loopy <smile.loopys2@gmail.com>
2024-08-31 23:11:34 -0700

    #21 - implement controller, service, view of admin user account page

--- src/main/java/com/personal/projectforumadmin/service/AdminAccountService.java
+++ src/main/java/com/personal/projectforumadmin/service/AdminAccountService.java
@@ -1,34 +1,46 @@
 package com.personal.projectforumadmin.service;
 
+import com.personal.projectforumadmin.domain.AdminAccount;
 import com.personal.projectforumadmin.domain.constant.RoleType;
 import com.personal.projectforumadmin.dto.AdminAccountDto;
 import com.personal.projectforumadmin.repository.AdminAccountRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
 import java.util.Set;
 
 @RequiredArgsConstructor
+@Transactional
 @Service
 public class AdminAccountService {
 
     private final AdminAccountRepository adminAccountRepository;
 
+    @Transactional
     public Optional<AdminAccountDto> searchUser(String username) {
-        return Optional.empty();
+
+        return adminAccountRepository.findById(username)
+                .map(AdminAccountDto::from);
     }
 
     public AdminAccountDto saveUser(String username, String password, Set<RoleType> roleTypes, String email, String nickname, String memo) {
-        return null;
+        return AdminAccountDto.from(
+                adminAccountRepository.save(AdminAccount.of(username, password, roleTypes, email, nickname, memo))
+        );
     }
 
+    @Transactional(readOnly = true)
     public List<AdminAccountDto> users() {
-        return List.of();
+
+        return adminAccountRepository.findAll().stream()
+                .map(AdminAccountDto::from)
+                .toList();
     }
 
     public void deleteUser(String username) {
-
+        adminAccountRepository.deleteById(username);
     }
 }
