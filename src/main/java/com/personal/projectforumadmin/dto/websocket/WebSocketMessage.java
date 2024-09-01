7b78f8e0c11175e3b537c77d01997adfa430948f
smile_loopy <smile.loopys2@gmail.com>
2024-09-01 12:28:30 -0700

    #28 - AdminLET Component + Implement chat using Spring WebSocket  example
    Apply spring websocket example to the chating component.

---
+++ src/main/java/com/personal/projectforumadmin/dto/websocket/WebSocketMessage.java
@@ -0,0 +1,7 @@
+package com.personal.projectforumadmin.dto.websocket;
+
+public record WebSocketMessage(String content) {
+    public static WebSocketMessage of(String content) {
+        return new WebSocketMessage(content);
+    }
+}
